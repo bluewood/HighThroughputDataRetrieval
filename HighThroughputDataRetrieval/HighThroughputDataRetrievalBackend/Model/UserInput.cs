1da590b580ec6948d65498028f07ccf81b7b039e
Hyesun Cha <hyesun.cha@wsu.edu>
2013-11-18 11:35:01 -0800

    Add missing

---
+++ HighThroughputDataRetrieval/HighThroughputDataRetrievalBackend/Model/UserInput.cs
@@ -0,0 +1,11 @@
+﻿namespace HighThroughputDataRetrievalBackend.Model
+{
+    public class UserInput
+    {
+        #region Properties
+        public string ProteinInModel { get; set; }
+        public string OrganismInModel { get; set; }
+        public string KeywordInModel { get; set; }
+        #endregion // Properties
+    }
+}
